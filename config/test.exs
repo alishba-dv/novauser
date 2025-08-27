a763bd20cf9530f53437ca8b397f66ed25ecc0fb
alishba-dv <alishbashabbir7677@gmail.com>
2025-08-27 11:58:15 +0500

    Iniitial commit

---
+++ config/test.exs
@@ -0,0 +1,56 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :data, Data.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "data_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :data, DataWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "1tvavCTCsNHuyVaoW5thF33djvtPJ9oowfeO4OO+XR+bgDLWXJ2XQtsWAnkXYPNW",
+  server: false
+
+# In test we don't send emails
+config :data, Data.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :api, ApiWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "LlD2q/nIa2iJqtlpFAlOFXtn2AiujdNxkA73cS2irelvM9exr5Pe1kOKGq0VqDWn",
+  server: false
+
+# In test we don't send emails
+config :api, Api.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
